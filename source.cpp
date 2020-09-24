8b88baea3526e64a1f8ba68e5ee81602d4692860
Mojtaba Abedi <68202630+s-mojtaba-a@users.noreply.github.com>
2020-09-24 16:07:47 +0330

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,25 +1,25 @@
 // recursive form
-int gcd(int a, int b, int& x, int& y) {
+template<typename T>
+T gcd(T a, T b, T& x, T& y) {
     if (b == 0) {
         x = 1;
         y = 0;
         return a;
     }
-    int x1, y1;
-    int d = gcd(b, a % b, x1, y1);
+    T x1, y1;
+    T d = gcd(b, a % b, x1, y1);
     x = y1;
     y = x1 - y1 * (a / b);
     return d;
 }
 
-
 // iterative form
-
-int gcd(int a, int b, int& x, int& y) {
+template<typename T>
+T gcd(T a, T b, T& x, T& y) {
     x = 1, y = 0;
-    int x1 = 0, y1 = 1, a1 = a, b1 = b;
+    T x1 = 0, y1 = 1, a1 = a, b1 = b;
     while (b1) {
-        int q = a1 / b1;
+        T q = a1 / b1;
         tie(x, x1) = make_tuple(x1, x - q * x1);
         tie(y, y1) = make_tuple(y1, y - q * y1);
         tie(a1, b1) = make_tuple(b1, a1 - q * b1);
